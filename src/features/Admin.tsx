bbdb1c30162e574101da108638c08fe480385875
Leandro Ysiano <lysianoortega@email.neit.edu>
2022-02-13 22:38:29 -0500

    Remove unused stuff

--- src/features/Admin.tsx
+++ src/features/Admin.tsx
@@ -6,6 +6,7 @@ import { Button, FlexBox, Stiches, Text } from "styles";
 import { Icons } from "styles/svg/ui-icons/icons";
 import AdminReports from "../components/admin/AdminReports";
 import AdminUsers from "../components/admin/AdminUsers";
+import { COLORS } from "utils/DEFS";
 
 const Admin = () => {
 
@@ -29,6 +30,7 @@ const Admin = () => {
                     onClick={OnClickProfile}
                 >
                     <img src={Icons.User} alt="User profile icon" />
+                    <Text color={COLORS.white}>Profile</Text>
                 </Button>
             </FlexBox>
             <Stiches margin=" 0.5rem 0 0 0" width="100%" />
