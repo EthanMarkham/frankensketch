484694849912a19b259aefdbe15a774102acb44e
Leandro Ysiano <lysianoortega@email.neit.edu>
2022-02-13 23:07:25 -0500

    commented out sw until prod

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,13 @@
-/* eslint-disable no-undef */
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', function (event) {
+    // do nothing here, just log all the network requests
+    //console.log(event.request.url);
+});
 
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
-);
+/* eslint-disable no-undef */
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+// workbox.setConfig({ debug: false });
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === 'image',
+//     new workbox.strategies.NetworkFirst()
+// );
