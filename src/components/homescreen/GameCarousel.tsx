9b135fb63f31a71058023f8ffe136b3b7289c6ac
Leandro Ysiano <lysianoortega@email.neit.edu>
2022-02-22 00:24:55 -0500

    Remove carousel dependency and fixed typo

--- src/components/homescreen/GameCarousel.tsx
+++ src/components/homescreen/GameCarousel.tsx
@@ -7,7 +7,6 @@ import { GraphQLResult } from "@aws-amplify/api-graphql";
 import React, { useState, useEffect } from "react";
 import { Div, Grid } from "styles";
 
-//TODO - Set PROPS and make dynamic
 function GameCarousel({ container }: GenericPageProps) {
     const [gameList, setGameList] = useState<Array<Game>>(new Array<Game>());
 
@@ -66,44 +65,4 @@ function GameCarousel({ container }: GenericPageProps) {
     );
 }
 
-/*
-
-
- <Carousel
-            show={2.5}
-            slide={2}
-            swiping={true}
-            infinite={false}
-            useArrowKeys={true}
-            responsive={true}
-            dynamic={true}
-            children={games}
-            leftArrow={
-                <FlexBox vhCenter={true} height="100%" width="1.25rem">
-                    <Button
-                        height="100%"
-                        width="1.25rem"
-                        fontSize="1.25em"
-                        background="none"
-                        color={COLORS.white}
-                    >
-                        &lt;
-                    </Button>
-                </FlexBox>
-            }
-            rightArrow={
-                <FlexBox vhCenter={true} height="125px" width="1.25rem">
-                    <Button
-                        height="4rem"
-                        width="1.25rem"
-                        fontSize="1.25em"
-                        background="none"
-                        color={COLORS.white}
-                    >
-                        &gt;
-                    </Button>
-                </FlexBox>
-            }
-        />
-*/
 export default GameCarousel;
