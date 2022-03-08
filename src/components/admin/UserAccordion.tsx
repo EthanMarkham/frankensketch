8cb914a8de22fc787a41d9c4a5692cfd2b54e343
Leandro Ysiano <lysianoortega@email.neit.edu>
2022-03-07 19:57:43 -0500

    Update user management

--- src/components/admin/UserAccordion.tsx
+++ src/components/admin/UserAccordion.tsx
@@ -1,21 +1,10 @@
 import { useState } from "react";
-import { Button, FlexBox, Text } from "styles";
+import { FlexBox, Text } from "styles";
 import { user } from "types/admin";
 import { COLORS } from "utils/DEFS";
 
-//TODO - add form fields and get user data
 const UserAccordion = (userData: user) => {
     const [isActive, setIsActive] = useState(false)
-
-
-    const saveChanges = () => {
-        alert("Save Changes()")
-    }
-
-    const cancelChanges = () => {
-        //TODO - add funcionality 
-        alert("Cancel Changes()")
-    }
 
     return (
         <>
@@ -29,14 +18,6 @@ const UserAccordion = (userData: user) => {
                         <Text fontWeight="300">ID: {userData.id}</Text>
                         <Text fontWeight="300" margin="0.5rem 0">Created At: {userData.createdAt}</Text>
                         <Text fontWeight="300">Updated At: {userData.updatedAt}</Text>
-                        <FlexBox direction="row" justifyContent="flex-end" margin="0.75rem 0 0 0">
-                            <Button margin="0 1rem" backgroundColor={COLORS.danger} padding=".25rem" width="4rem" onClick={cancelChanges}>
-                                <Text color={COLORS.white}>cancel</Text>
-                            </Button>
-                            <Button backgroundColor={COLORS.blue} padding=".25rem" width="4rem" onClick={saveChanges}>
-                                <Text color={COLORS.white}>save</Text>
-                            </Button>
-                        </FlexBox>
                     </FlexBox>
                 )}
             </FlexBox>
